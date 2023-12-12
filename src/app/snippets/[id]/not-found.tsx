14aeff1d6bba7bd1f7b1d708e6a10ce684fe262a
Tanaka Shoko <realtanakashoko@gmail.com>
2023-12-12 16:00:22 +0200

    update 2

---
+++ src/app/snippets/[id]/not-found.tsx
@@ -0,0 +1,8 @@
+export default function SnippetNotFound() {
+    return (
+        <div>
+            <h1 className="text-xl bold">Sorry, but we couldn&apos;t find that particular snippet</h1>
+            
+        </div>
+    );
+}
--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,7 +1,31 @@
+'use client';
+import { useFormState } from "react-dom";
+import * as actions from '@/actions';
+
 export default function SnippetCreatePage() {
+    
+    const [formState, action] = useFormState(actions.createSnippet, {message: ""});
     return (
         <div>
-            Create a snippet
+            <form action={action}>
+                <h3 className="font-bold m-3">
+                    Create a snippet
+                </h3>
+                <div className="flex flex-col gap-4">
+                    <div className="flex gap-4">
+                        <label className="w-12"  htmlFor="title">Title</label>
+                        <input name="title" className="border rounded p-2 w-full" id="title"/>
+                    </div>
+                    <div className="flex gap-4">
+                        <label className="w-12"  htmlFor="code">Code</label>
+                        <textarea name="code" id="code" className="border rounded p-2 w-full" />
+                    </div>
+                    {
+                        formState.message ? <div className="my-2 p-2 bg-red-200 border rounded border-red-400">{formState.message}</div> : null
+                    }
+                    <button type="submit" className="rounded p-2 bg-blue-200">Create</button>
+                </div>
+            </form>
         </div>
     );
 }
